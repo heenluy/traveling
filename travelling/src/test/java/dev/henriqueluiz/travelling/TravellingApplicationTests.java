15e528ea63d2d1a357620f9187b663b98fe600c2
Henrique Luiz <heenluy@gmail.com>
2022-12-15 15:53:29 -0300

    build: initail configuration

---
+++ travelling/src/test/java/dev/henriqueluiz/travelling/TravellingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.henriqueluiz.travelling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravellingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
