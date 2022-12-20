5b5b34a7ba4d2c9ec61bf073008572e8d5d195ef
Henrique Luiz <heenluy@gmail.com>
2022-12-19 21:58:20 -0300

    feat(user): creates user entities

---
+++ travelling/src/main/java/dev/henriqueluiz/travelling/model/AppRole.java
@@ -0,0 +1,73 @@
+package dev.henriqueluiz.travelling.model;
+
+import static jakarta.persistence.GenerationType.SEQUENCE;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppRole {
+    
+    @Id
+    @GeneratedValue(
+        strategy = SEQUENCE,
+        generator = "role_seq"
+    )
+    @SequenceGenerator(
+        name = "role_seq",
+        sequenceName = "role_id_seq",
+        allocationSize = 1
+    )
+    private Long roleId;
+
+    @NotBlank
+    private String name;
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((roleId == null) ? 0 : roleId.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        AppRole other = (AppRole) obj;
+        if (roleId == null) {
+            if (other.roleId != null)
+                return false;
+        } else if (!roleId.equals(other.roleId))
+            return false;
+        return true;
+    }
+}
---
+++ travelling/src/main/java/dev/henriqueluiz/travelling/model/AppUser.java
@@ -0,0 +1,127 @@
+package dev.henriqueluiz.travelling.model;
+
+import static jakarta.persistence.FetchType.EAGER;
+import static jakarta.persistence.GenerationType.SEQUENCE;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppUser {
+    
+    @Id
+    @GeneratedValue(
+        strategy = SEQUENCE,
+        generator = "user_seq"
+    )
+    @SequenceGenerator(
+        name = "user_seq",
+        sequenceName = "user_id_seq",
+        allocationSize = 1
+    )
+    private Long userId;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    @Length(min = 6)
+    private String password;
+
+    @ManyToMany(fetch = EAGER)
+    private Collection<AppRole> authorities = new ArrayList<>();
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+    
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Collection<AppRole> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Collection<AppRole> authorities) {
+        this.authorities = authorities;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((userId == null) ? 0 : userId.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        AppUser other = (AppUser) obj;
+        if (userId == null) {
+            if (other.userId != null)
+                return false;
+        } else if (!userId.equals(other.userId))
+            return false;
+        return true;
+    }
+}
