bfb13c50d95992636c29dcd3324d0fd971ef8bf1
Henrique Luiz <heenluy@gmail.com>
2022-12-25 19:11:42 -0300

    build(test): updates test configuration

---
+++ travelling/src/test/resources/dev/henriqueluiz/travelling/repository/insertUser.sql
@@ -0,0 +1,2 @@
+INSERT INTO app_user (user_id, first_name, last_name, email, password)
+VALUES (nextval('user_id_seq'), 'Henrique', 'Luiz', 'test@mail.dev', 'developer');
---
+++ travelling/src/test/resources/schema.sql
@@ -0,0 +1,31 @@
+-- CREATES ROLE TABLE
+DROP SEQUENCE IF EXISTS role_id_seq CASCADE;
+CREATE SEQUENCE role_id_seq;
+DROP TABLE IF EXISTS app_role;
+
+CREATE TABLE app_role (
+    role_id INT8 NOT NULL DEFAULT nextval('role_id_seq'),
+    name VARCHAR(16) NOT NULL,
+    PRIMARY KEY(role_id)
+);
+
+ALTER SEQUENCE role_id_seq
+OWNED BY app_role.role_id;
+
+-- CREATES USER TABLE
+DROP SEQUENCE IF EXISTS user_id_seq CASCADE;
+CREATE SEQUENCE user_id_seq;
+DROP TABLE IF EXISTS app_user;
+
+CREATE TABLE app_user (
+    user_id INT8 NOT NULL DEFAULT nextval('user_id_seq'),
+    first_name VARCHAR(32) NOT NULL,
+    last_name VARCHAR(32) NOT NULL,
+    email VARCHAR(48) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    authorities TEXT[],
+    PRIMARY KEY(user_id)
+);
+
+ALTER SEQUENCE user_id_seq
+OWNED BY app_user.user_id;
