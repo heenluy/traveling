d0463163458a48bae39de42d24c6e28ea383bf0c
Henrique Luiz <heenluy@gmail.com>
2022-12-25 19:14:41 -0300

    test(user):  test all repositories

---
+++ travelling/src/test/java/dev/henriqueluiz/travelling/repository/RoleRepoTest.java
@@ -0,0 +1,31 @@
+package dev.henriqueluiz.travelling.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import dev.henriqueluiz.travelling.model.AppRole;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = NONE)
+public class RoleRepoTest {
+    
+    @Autowired
+    private RoleRepo repository;
+
+    @Test
+    @Sql(scripts = "insertRole.sql", executionPhase = BEFORE_TEST_METHOD)
+    @Sql(scripts = "deleteRole.sql", executionPhase = AFTER_TEST_METHOD)
+    void givenRoleName_whenRun_thenRoleEntityIsExpected() {
+        AppRole role = repository.findByName("test").orElseThrow();
+        assertThat(role.getRoleId()).isNotNull();
+        assertThat(role.getName()).isEqualTo("test");
+    }
+}
---
+++ travelling/src/test/java/dev/henriqueluiz/travelling/repository/UserRepoTest.java
@@ -0,0 +1,31 @@
+package dev.henriqueluiz.travelling.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import dev.henriqueluiz.travelling.model.AppUser;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = NONE)
+public class UserRepoTest {
+    
+    @Autowired
+    private UserRepo repository;
+
+    @Test
+    @Sql(scripts = "insertUser.sql", executionPhase = BEFORE_TEST_METHOD)
+    @Sql(scripts = "deleteUser.sql", executionPhase = AFTER_TEST_METHOD)
+    void givenUserEmail_whenRun_thenUserEntityIsExpected() {
+        AppUser user = repository.findByEmail("test@mail.dev").orElseThrow();
+        assertThat(user.getUserId()).isNotNull();
+        assertThat(user.getEmail()).isEqualTo("test@mail.dev");
+    }
+}
